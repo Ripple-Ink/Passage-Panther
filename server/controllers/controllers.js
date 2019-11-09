bbb903c7e4f07a39fbcf0134d1ddeb505cf6f5ee
Andrew Shin <andyrpg@gmail.com>
2019-11-09 15:18:46 -0800

    ADDED MIDDLEWARE IN CONTROLLER

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -22,7 +22,7 @@ controller.getPassage = (req, res, next) => {
     db.query(text)
     .then(data => data.json())
     .then(result => { 
-        console.log(result) //probably need a conditional to check if the _ids matches, but not sure what the result object looks like.
+        console.log(result) 
         return res.status(200).json({result})
         })
     .catch(err  => {
@@ -34,10 +34,13 @@ controller.createAccount = (req, res, next) => {
     const {username, password} = req.body; 
     const text = 'INSERT INTO logininfo (username, password) VALUES ($1, $2)'
     const values = [username, password];
+    //Need to check if the req body username !== username. Need to find out whaat the result object
 
     db.query(text, values, (err, result) => {
         if (!values[0] || !values[1]) { 
             return res.json({message: "please enter a username or password!"})
+        } else { 
+            return res.status(200).json(result)
         }
     })
 }
