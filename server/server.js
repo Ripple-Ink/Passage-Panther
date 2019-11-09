749b67512fdfa79ccc46103935eab6143b7a3cca
Scott McInnis <scottalyst@gmail.com>
2019-11-09 14:39:25 -0800

    for testing purposes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
+const dataController = require('./controllers/controllers.js');
+
+	//
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.use(bodyParser());
@@ -13,7 +16,26 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.get('/getTitles', (req, res) => {});
+app.get('/getTitles',
+	dataController.getAllTitles,
+	(req, res) => {});
 
+app.get('/getPassage/:id',
+	dataController.getPassage, 
+	(req, res) => {});
+
+app.post('/newPassage', (req, res) => {});
+
+	// Error Handling -------------------------------------
+app.use('*', (req, res) => {
+	res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).send('Internal Server Error');
+});
+
+	// Start up Server ------------------------------------
 app.listen(PORT, ()=>{console.log(`server running on ${PORT}`)})
 module.exports = app;
