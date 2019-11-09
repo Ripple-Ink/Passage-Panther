011ffa7ccf8024ba2daf528eb4f025b07291b7c7
choies921003 <choies921003@gmail.com>
2019-11-09 15:18:20 -0800

    minor change for header

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "./components/Header.jsx";
 import WriteNewStory from "./components/WriteNewStory.jsx";
-import NewStoryButton from "./components/NewStoryButton.jsx"
-import Login from './components/Login.jsx';
+import NewStoryButton from "./components/NewStoryButton.jsx";
+import Login from "./components/Login.jsx";
 import TitleFeed from "./components/TitleFeed.jsx";
 
 class App extends React.Component {
@@ -12,8 +12,8 @@ class App extends React.Component {
       username: "",
       password: "",
       isUpload: false,
-      titles: [{id: 1, title: 'scott', author:'mactruck'}],
-      passages: [] 
+      titles: [{ id: 1, title: "scott", author: "mactruck" }],
+      passages: []
     };
     // this.login = this.login.bind(this);
     // this.onChangeUsername = this.onChangeUsername.bind(this);
@@ -58,7 +58,8 @@ class App extends React.Component {
     return (
       <div>
         <Header /> {/* header is render Passages and login button */}
-        {this.state.isUpload ? <WriteNewStory /> : <NewStoryButton/>}{/* if  */}
+        {this.state.isUpload ? <WriteNewStory /> : <NewStoryButton />}
+        {/* if  */}
         <TitleFeed titles={this.state.titles} />
       </div>
     );
@@ -67,14 +68,13 @@ class App extends React.Component {
 
 export default App;
 
-
 // id , title, author, content, genre, parent, child1, child2, summary
 
 //props title
 
-  //note ask!
-  // <Login
-  //   onChangeUsername={this.onChangeUsername}
-  //   onChangePassword={this.onChangePassword}
-  //   loginButton={this.loginButton}
-  // />
+//note ask!
+// <Login
+//   onChangeUsername={this.onChangeUsername}
+//   onChangePassword={this.onChangePassword}
+//   loginButton={this.loginButton}
+// />
