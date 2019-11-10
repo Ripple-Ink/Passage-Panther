d85bc509c94815ed19b8eb88cc01b8da15b4191f
Andrew Shin <andyrpg@gmail.com>
2019-11-09 17:06:55 -0800

    Tested middleware for getPassage and getAllTitles and they work. Also editied SQL table

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const dataController = require('./controllers/controllers.js')
 
 const app = express();
 const PORT = 3000;
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-app.use(bodyparser());
+app.use(bodyParser.json());
 
 
 
@@ -15,8 +16,18 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.get('/getTitles', (req, res) => {});
+app.get('/getTitles', dataController.getAllTitles);
+app.get('/getPassage/:id', dataController.getPassage);
+app.post('/createAccount', dataController.createAccount);
 
+app.use('*', (req, res) => {
+	res.status(404).send("not found")
+})
+
+app.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).send("internal server error")
+})
 app.listen(PORT, ()=>{console.log(`server running on ${PORT}`)})
 
 module.exports = app;
