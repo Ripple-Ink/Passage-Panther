76862ac50f6df84e669677071e9b796c5527e31f
justin1j <wonjun110@gmail.com>
2019-11-11 10:31:22 -0800

    updated upload

--- client/components/UploadNewStory.jsx
+++ client/components/UploadNewStory.jsx
@@ -8,7 +8,7 @@ const UploadNewStory = props => {
       Content: <input name="content" type="text" onChange={props.onChangeInput}/>
       Path 1: <input name="path1" type="text" onChange={props.onChangeInput}/>
       Path 2: <input name="path2" type="text" onChange={props.onChangeInput}/>
-      <div onClick={props.handleUploadPassage}>Submit</div>
+      <div onClick={props.handleUploadPassage()}>Submit</div>
     </div>
   );
 };
